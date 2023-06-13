1e6afa321c3017e501b456d6fe30bcee41bf516b
shoavid <shoavid@gmail.com>
2023-06-13 12:38:27 +0300

    union find 💀

--- unionFind.h
+++ unionFind.h
@@ -1,7 +1,7 @@
 #ifndef UNION_FIND
 #define UNION_FIND
-using namespace std;
 #include <cassert>
+#include <memory>
 
 
 class Group{
@@ -26,13 +26,13 @@ template<class T>
 class Element{
     private:
         int ID;
-        int distanceFromRoot = 0;;
-        Group* group;
+        int distanceFromRoot = 0;
+        std::shared_ptr<Group> group;
         Element<T>* next;
         T data;
     public:
         Element(int id, T data, int groupID): ID(id), distanceFromRoot(0), group(nullptr), next(nullptr), data(data){
-            group = new Group(groupID);
+            group = std::shared_ptr<Group>(new Group(groupID));
         }
 
         bool isRoot(){
@@ -44,7 +44,6 @@ class Element{
                 if (next->isRoot()){
                     next->group->addGroupCount(group->getGroupCount());
                 }
-                delete group;
             }
             this->next = next;
             distanceFromRoot = next->distanceFromRoot+1;
@@ -62,7 +61,7 @@ class Element{
             return distanceFromRoot;
         }
 
-        Group* getGroup(){
+        std::shared_ptr<Group> getGroup(){
             Element<T>* Root = getRoot();
             return Root->group;
         }
@@ -73,37 +72,31 @@ class Element{
 };
 
 
-template<class T>
+template<class T, int N>
 class UnionFind{
     private:
-        Element<T>* elements;
+        std::shared_ptr<Element<T> > elements[N];
         int amount;
 
-        void unionhelper(Element<T>* elem, Element<T>* root){
+        void unionhelper(std::shared_ptr<Element<T> > elem, std::shared_ptr<Element<T> > root){
             elem->setNext(root);
         }
 
     public:
         UnionFind(int amount, T* t){
-            this->elements = new Element<T>*[amount];
             for (int i=0; i<amount; i++){
-                this->elements[i] = new Element<T>(i, t[i], i);
+                elements[i] = std::shared_ptr<Element<T> >(new Element<T>(i, t[i], i));
             }
             this->amount = amount;
         }
 
         ~UnionFind(){
-            for (int i=0; i<amount; i++){
-                if (elements[i]->isRoot()){
-                    delete elements[i]->getGroup();
-                }
-                delete elements[i];
-            }
+            delete[] elements;
         }
 
         int find(int elementID){
             Element<T>* root = elements[elementID]->getRoot();
-            Element<T>* it = elements[elementID];
+            Element<T>* it = elements[elementID].get();
             while (!it->isRoot()){
                 it->setNext(root);
                 it = it->getNext();
